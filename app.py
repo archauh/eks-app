5c0a058972a7a6761840f8b816cf1082393cc6b6
Anand Chauhan <91910507+archauh@users.noreply.github.com>
2025-07-29 16:58:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from my EKS app!"
+    return "Hello from my CICD EKS app!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
